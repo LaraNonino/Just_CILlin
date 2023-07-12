1ce2a3ad078411b43e519079b5429db3817eff6e
Anisha Mohamed <anishamohamed@Anishas-MacBook-Pro.local>
2023-07-12 11:56:31 +0100

    update attention

--- models/attention.py
+++ models/attention.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import math
 
 class SelfAttention(nn.Module):
-    def __init__(self, embed_dim):
+    def __init__(self, embed_dim, collapse=False):
         super(SelfAttention, self).__init__(embed_dim)
 
         self.embed_dim = embed_dim
@@ -14,21 +14,27 @@ class SelfAttention(nn.Module):
         self.W_k = nn.Linear(self.embed_dim, self.embed_dim, bias=False)
         self.W_v = nn.Linear(self.embed_dim, self.embed_dim, bias=False)
 
+        self.collapse = collapse
+
     def forward(self, x: torch.Tensor):
         Q = self.W_q(x)
         K = self.W_k(x)
         V = self.W_v(x)
 
         H = self._scaled_dot_product_attention(Q, K, V) # H: (batch_size, embed_dim)
-        # c = torch.sum(H, dim=1) # c: context vector
-
+        
+        if self.collapse:
+            c = torch.sum(H, dim=1) # c: context vector
+            return c
         return H
     
     def _scaled_dot_product_attention(self, Q, K, V):
 
         # scale by embed_dim so that the softmax doesn't saturate
-        Q = torch.div(Q, math.sqrt(self.embed_dim))
-        scores = torch.bmm(Q, torch.transpose(K, -2, -1))
+        scores = torch.div(
+            torch.bmm(Q, torch.transpose(K, -2, -1)),
+            math.sqrt(self.embed_dim)
+        )
 
         A = F.softmax(scores, dim=-1)
         H = torch.bmm(A, V)
