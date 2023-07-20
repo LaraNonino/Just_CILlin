07b5f25ec8da51f81876b2316d9ffb2cf7cfb60a
Anisha Mohamed <anishamohamed@student-net-vpn-1859.intern.ethz.ch>
2023-07-20 07:43:21 +0200

    selfattention3d

--- models/attention.py
+++ models/attention.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 import math
 
-class SelfAttention(nn.Module):
+class SelfAttention3D(nn.Module):
     def __init__(
         self, 
         embed_dim, 
@@ -25,10 +25,54 @@ class SelfAttention(nn.Module):
         self.collapse = collapse
 
     def forward(self, x: torch.Tensor):
-        # x: (batch_size, seq_length, embed_dim) V (batch_size, embed_dim)
-        Q = self.W_q(x)
-        K = self.W_k(x)
-        V = self.W_v(x)
+        # x: (batch_size, seq_length, embed_dim)
+        Q = self.W_q(x) # Q: (batch_size, seq_len, q_dim)
+        K = self.W_k(x) # K: (batch_size, seq_len, k_dim=q_dim)
+        V = self.W_v(x) # V: (batch_size, seq_len, v_dim)
+        
+        H = self._scaled_dot_product_attention(Q, K, V) 
+        
+        if self.collapse:
+            c = torch.sum(H, dim=1) # c: context vector
+            return c
+        return H
+    
+    def _scaled_dot_product_attention(self, Q, K, V):
+        scores = torch.div(
+            torch.matmul(Q, torch.transpose(K, -2, -1)), 
+            math.sqrt(self.q_dim)
+        ) # scale by embed_dim so that the softmax doesn't saturate
+
+        A = F.softmax(scores, dim=-1) # A: (batch_size, seq_len, seq_len)
+        H = torch.bmm(A, V) #  H: (batch_size, seq_len, v_dim)
+        
+        return H
+    
+class SelfAttention2D(nn.Module):
+    def __init__(
+        self, 
+        embed_dim, 
+        q_dim=768,
+        v_dim=768,
+        collapse=False
+    ):
+        super().__init__()
+
+        self.embed_dim = embed_dim
+        self.q_dim = q_dim
+        self.v_dim = v_dim
+        
+        self.W_q = nn.Linear(self.embed_dim, self.q_dim, bias=False)
+        self.W_k = nn.Linear(self.embed_dim, self.q_dim, bias=False)
+        self.W_v = nn.Linear(self.embed_dim, self.v_dim, bias=False)
+
+        self.collapse = collapse
+
+    def forward(self, x: torch.Tensor):
+        # x: (batch_size, embed_dim)
+        Q = self.W_q(x) # Q: (batch_size, q_dim)
+        K = self.W_k(x) # K: (batch_size, k_dim=q_dim)
+        V = self.W_v(x) # V: (batch_size, v_dim)
         
         H = self._scaled_dot_product_attention(Q, K, V) # H: (batch_size, embed_dim)
         
@@ -38,15 +82,23 @@ class SelfAttention(nn.Module):
         return H
     
     def _scaled_dot_product_attention(self, Q, K, V):
-        if len(Q.shape) == 2: # x:(batch_size, embed_dim)
-            prod = torch.bmm(Q.unsqueeze(1), K.unsqueeze(-1))
-        else: # x: (batch_size, seq_length, embed_dim)
-            prod = torch.bmm(Q, torch.transpose(K, -2, -1))
+        # x: (batch_size, seq_length, embed_dim)
+        scores = torch.div(
+            torch.matmul(Q, torch.transpose(K), -2, -1), 
+            math.sqrt(self.q_dim)
+        ) # scale by embed_dim so that the softmax doesn't saturate
 
-        scores = torch.div(prod, math.sqrt(self.embed_dim)) # scale by embed_dim so that the softmax doesn't saturate
-
-        A = F.softmax(scores, dim=-1)
-        print(f"A:{A.shape}")
-        H = torch.bmm(A, V)
-        print(f"A:{A.shape}, scores:{scores.shape}, H:{H.shape}")
+        A = F.softmax(scores, dim=-1) # A: (batch_size, seq_len, seq_len)
+        H = torch.bmm(A, V) #  H: (batch_size, seq_len, v_dim)
+        
         return H
+    
+# x = torch.ones((10, 7, 16))
+# att = SelfAttention3D(16, q_dim=30, v_dim=8)
+# x = att(x)
+# print(x.shape)
+
+# x = torch.ones((10, 16))
+# att = SelfAttention2D(16, q_dim=30, v_dim=8)
+# x = att(x)
+# print(x.shape)
