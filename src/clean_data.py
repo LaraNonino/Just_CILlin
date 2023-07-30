b50f7a4a4386d0de1f34b737dc94e7ee05b2e083
Elisa <elisamecp@gmail.com>
2023-07-30 17:41:19 +0200

    fix clean data

--- src/clean_data.py
+++ src/clean_data.py
@@ -2,15 +2,14 @@ from dataset.twitter_dataset import TwitterDataModule
 from preprocessing.tokenize import Tokenizer
 
 def main():
-    positive = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/train_pos_full.txt", stage="fit")
-    negative = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/train_neg_full.txt", stage="fit")
-    t = Tokenizer(save_to_file="train_tokenized.txt")
+    positive = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/train_pos.txt", stage="fit")
+    negative = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/train_neg.txt", stage="fit")
+    t = Tokenizer(save_to_file="twitter-datasets/train_tokenized.txt")
     t(positive + negative)
 
-    # test = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/test_data.txt", stage="predict")
-    # print(len(test))
-    # t = Tokenizer(save_to_file="twitter-datasets/test_tokenized.txt")
-    # t(test)
+    test = TwitterDataModule._load_tweets(TwitterDataModule, "twitter-datasets/test_data.txt", stage="predict")
+    t = Tokenizer(save_to_file="twitter-datasets/test_tokenized.txt")
+    t(test)
 
 if __name__=="__main__":
     main()
